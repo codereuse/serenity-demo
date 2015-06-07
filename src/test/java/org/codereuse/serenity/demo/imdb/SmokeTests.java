6c42ddf2d9d1fe74858934353510692a7e757881
slawert <slawert@gmail.com>
2015-06-07 19:47:58 +0200

    Added some different types of test outputs

--- src/test/java/org/codereuse/serenity/demo/imdb/SmokeTests.java
+++ src/test/java/org/codereuse/serenity/demo/imdb/SmokeTests.java
@@ -5,6 +5,7 @@ import net.thucydides.core.annotations.Managed;
 import net.thucydides.core.annotations.Screenshots;
 import net.thucydides.core.annotations.Steps;
 import org.codereuse.serenity.demo.imdb.steps.UserSteps;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.openqa.selenium.WebDriver;
@@ -29,5 +30,20 @@ public class SmokeTests {
         user.search_header_should_contain("Titanic");
         user.should_see_number_of_titles_displayed(10);
         user.should_see_in_search_results("Titanic (1997)");
+    }
+
+    @Test
+    @Ignore
+    public void shouldBeIgnored(){
+        //This method is intended to be empty as it is going to be ignored.
+    }
+
+    @Test
+    @Screenshots(afterEachStep = true)
+    public void shouldFail(){
+        user.opens_imdb_home_page();
+        user.searches_for_movie("Titanic");
+        //This line should make the test fail.
+        user.search_header_should_contain("Godfather");
     }
 }
